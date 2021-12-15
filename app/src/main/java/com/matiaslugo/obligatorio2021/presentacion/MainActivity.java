872ab701d2d634fb51c5f0ee5b3bae9342550af7
MatiasNLugo <74388797+MatiasNLugo@users.noreply.github.com>
2021-12-15 00:00:51 -0300

    Agregado Logo

--- app/src/main/java/com/matiaslugo/obligatorio2021/presentacion/MainActivity.java
+++ app/src/main/java/com/matiaslugo/obligatorio2021/presentacion/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,70 +18,12 @@ import com.matiaslugo.obligatorio2021.viewseventos.ListarEventosFragment;
 
 public class MainActivity extends AppCompatActivity{
 
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private Button btnAgregar, btnAgregarEvento, btnListarEvento;
-    private TextView tv;
-    private String paraMandar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-        btnAgregar = findViewById(R.id.btnAgregarCliente);
-        btnAgregarEvento = findViewById(R.id.btnAgregarEvento);
-        btnListarEvento = findViewById(R.id.btnIrListarEventos);
-        tv = findViewById(R.id.tv);
-
-    btnAgregar.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            try{
-                IrAclientes();
-            } catch (Exception ex) {
-                String mensaje = ex.toString();
-                Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
-            } finally {
-                //dbClientes.close();
-            }
-        }
-    });
-
-    btnAgregarEvento.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            btnOnClickAgregarEvento(v);
-        }
-    });
-
-    btnListarEvento.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            btnOnClickListarEventos(v);
-        }
-    });
-
     }
-
-    protected void IrAclientes(){
-        Intent enviar = new Intent(this, ClienteMantenimiento.class);
-        startActivity(enviar);
-    }
-
-
-    protected void btnOnClickAgregarEvento(View view) {
-
-
-    }
-
-
-    protected void btnOnClickListarEventos(View view) {
-        Intent enviar = new Intent(this, ListarEventosFragment.class);
-        startActivity(enviar);
-    }
-
 
 }
